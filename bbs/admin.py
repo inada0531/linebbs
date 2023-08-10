2c69d6a32d71e78a1fa34b0e5c8bd5568417731b
inada0531 <dier_wolf3@yahoo.co.jp>
2023-08-10 16:22:53 +0000

    second commit

--- bbs/admin.py
+++ bbs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from bbs.models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+#実際に管理サイトに登録している
+admin.site.register(Post, PostAdmin)
 
 # Register your models here.
--- bbs/views.py
+++ bbs/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from bbs.models import Post
+from django.http import HttpResponse
+from bbs.forms import PostForm
 
 def home_view(request):
+    #bbs/のテンプレートを出力するビュー
+    
+    #webページが要求してきたリクエストで分岐
+    if request.method == 'GET':
+        return home_view_get(request)
+    elif request.method == 'POST':
+        return home_view_post(request)
+    else:
+        return HttpResponse('invalid method', status=400)
+
+def home_view_get(request, form=None):
+    #bbs/のGET
     context = {}
 
     context['title'] = '一行掲示板'
@@ -10,5 +24,27 @@ def home_view(request):
     #レコードをDjangoのオブジェクトに変換する
     #<=>DBへのクエリを取得している<=>掲示板の利用者の投稿をすべて取得
 
+    if form:
+        context['form'] = form #form引数がNoneじゃなければその引数をコンテキストに
+    else:
+        context['form'] = PostForm() #フォームを保存？
+
+    # renderにコンテキストを渡しテンプレートを描画
     return render(request, 'bbs/home.html', context)
+
+def home_view_post(request):
+    #bbs/のPOST
+
+    form = PostForm(request.POST)
+
+    #入力エラー時に getを呼び出してコンテキストに保存させている
+    if not form.is_valid():
+        return home_view_get(request, form=form)
+    
+    form.save()
+    #ブラウザのリロードによるフォームからの2重送信を防ぐための処置
+    return redirect('bbs_home')
+    #↓これだとだめ
+    #return home_view_get(request, form=form)
+
 # Create your views here.
